c156142dceb53af48cd93f1fb9e4ba5091e1af6f
VitorioBarreto <vitorio.augusto.silva@gmail.com>
2025-07-23 14:04:10 -0300

    commit inicial

---
+++ Microservices/Courier-management/src/test/java/com/vitoriobarreto/algadelivery/courier/management/CourierManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitoriobarreto.algadelivery.courier.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourierManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
